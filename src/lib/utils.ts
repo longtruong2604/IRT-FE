5c1319871aa84bc9eab975697b0034015052af5d
longtruong2604 <longtruong2604@gmail.com>
2024-10-23 22:14:05 +0700

    chore: add shadcn ui

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,18 +1,6 @@
-import { ItemData } from '../types/response_data.type'
-import { TableData } from '../types/table_data.type'
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
-export const transformData = (data: ItemData) => {
-  const excludeKeys = ['question', 'p_value', 'rpbis', 'difficulty']
-  return Object.entries(data)
-    .filter(([key, _]) => !excludeKeys.includes(key))
-    .map(([key, value]): TableData => {
-      return {
-        label: key,
-        ...value,
-      }
-    })
-}
-
-export const normalizePath = (path: string) => {
-  return path.startsWith('/') ? path.slice(1) : path
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
