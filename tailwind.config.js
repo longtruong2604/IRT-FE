f96a369e88a6f441b939f66ae18ec82ff0a63807
longtruong2604 <longtruong2604@gmail.com>
2024-12-23 12:27:50 +0700

    fix: fix build error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
-/** @type {import('tailwindcss').Config} */
+import tailwindAnimate from 'tailwindcss-animate'
+
 export default {
   darkMode: ['class'],
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
@@ -67,5 +68,5 @@ export default {
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [tailwindAnimate],
 }
